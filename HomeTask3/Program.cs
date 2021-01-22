a7f073e1efe92c9d998cb0b7c097e135f9e1b333
emedvedeva <35913899+emedvedeva@users.noreply.github.com>
2021-01-22 16:28:46 +0300

    Add HomeTask 2, 3, 4

---
+++ HomeTask3/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace HomeTask3
+{
+    class Program
+    {
+        /*Записаня книжка
+        В себе содержит:
+            Имя, номер телефона, возраст
+        Функционал: 
+            просмотр книжки
+            добавление записи
+            удаление записи */
+
+        static void Main(string[] args)
+        {
+            string[] names = new string[3] { "Роман", "Максим", "Александр" };
+            long[] phoneNumbers = new long[3] { 9991110001, 9991110002, 9991110003 };
+            byte[] ages = new byte[3] { 24, 23, 27 };
+            int entitiesCount = names.Length;
+            bool shouldContinue = true;
+
+            while (shouldContinue == true)
+            {
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("для просморта записной книжки нажмите (1)");
+                Console.WriteLine("для добавления записи в книжку нажмите (2)");
+                Console.WriteLine("для удаления записи в кнжику нажмите (3)");
+                Console.WriteLine("длы выхода нажмите (0)");
+
+                string userAnswer = Console.ReadLine();
+
+                switch (userAnswer)
+                {
+                    case "1":                                                                       //Просмотр книжки
+                        Console.WriteLine("Порядковый номер - Имя - номер телефона - возраст");
+                        for (int i = 0; i < names.Length; i++)
+                        {
+                            Console.WriteLine($" {i + 1} - {names[i]} - {phoneNumbers[i]} - {ages[i]}");
+                        }
+                        break;
+                    case "2":                                                                       //Добалвение новой записи в книжку
+                        entitiesCount++;
+
+                        Array.Resize(ref names, entitiesCount);
+                        Console.WriteLine("Введите имя");
+
+                        names[entitiesCount - 1] = Console.ReadLine();
+
+                        Array.Resize(ref phoneNumbers, entitiesCount);
+                        Console.WriteLine("Введите номер телефона");
+
+                        phoneNumbers[entitiesCount - 1] = Int64.Parse(Console.ReadLine());
+
+                        Array.Resize(ref ages, entitiesCount);
+                        Console.WriteLine("Введите возраст");
+
+                        ages[entitiesCount - 1] = Byte.Parse(Console.ReadLine());
+
+                        break;
+
+                    case "3":                                                                           //Удаление записи из книжки
+                        bool shouldRemove = true;
+
+                        do
+                        {
+                            for (int i = 0; i < names.Length; i++)
+                            {
+                                Console.WriteLine($" {i + 1} - {names[i]} - {phoneNumbers[i]} - {ages[i]}");
+                            }
+
+                            Console.WriteLine("Для удаления записи введите порядковый номер записи");
+
+                            int numberOfContact = Int32.Parse(Console.ReadLine());
+
+                            if ((0 < numberOfContact) && (numberOfContact <= entitiesCount))
+                            {
+                                for (int i = numberOfContact; i < names.Length; i++)
+                                {
+                                    names[i - 1] = names[i];
+                                    phoneNumbers[i - 1] = phoneNumbers[i];
+                                    ages[i - 1] = ages[i];
+                                }
+
+                                entitiesCount--;
+
+                                Array.Resize(ref names, entitiesCount);
+                                Array.Resize(ref phoneNumbers, entitiesCount);
+                                Array.Resize(ref ages, entitiesCount);
+                                Console.WriteLine($"Вы успешно удалили запись номер {numberOfContact} из книжки");
+
+                                shouldRemove = false;
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Вы ввели несуществующий номер");
+                                Console.WriteLine("Выйти из функции удаления записей? (y/n) ");
+
+                                string answer = Console.ReadLine();
+
+                                if (answer == "y")
+                                {
+                                    shouldRemove = false;
+                                }
+
+                            }
+                        } while (shouldRemove);
+
+                        break;
+
+                    case "0": //выход
+                        shouldContinue = false;
+
+                        return;
+
+                    default:
+                        Console.WriteLine("Вы нажали неизветсную цифру");
+
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ HomeTask4/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace HomeTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double c;
+            bool shouldContinue = true;
+
+            do
+            {
+                a = GetNumber();
+                b = GetNumber();
+
+                Console.WriteLine("Выберите операцию:");
+                Console.WriteLine("для сложения чисел нажмите (1)");
+                Console.WriteLine("для вычитания чисел нажмите (2)");
+                Console.WriteLine("для умножения чисел нажмите (3)");
+                Console.WriteLine("для деления чисел нажмите (4)");
+                Console.WriteLine("для выхода нажмите (0)");
+
+                string operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "0":
+                        return;
+                    case "1":
+                        Console.WriteLine("a + b = " + (a + b));
+                        break;
+                    case "2":
+                        Console.WriteLine("a - b = " + (a - b));
+                        break;
+                    case "3":
+                        Console.WriteLine("a * b = " + (a * b));
+                        break;
+                    case "4":
+                        Console.WriteLine("a / b = " + (a / b));
+                        break;
+                    default:
+                        Console.WriteLine("Вы нажали неизвестную цифру");
+                        break;
+                }
+
+                Console.WriteLine("Хотите продожить? (y/n)");
+                string answer = Console.ReadLine();
+                if (answer == "n")
+                {
+                    shouldContinue = false;
+                }
+                else if (answer!="y")
+                {
+                    Console.WriteLine("Вы ввели неизместную букву");
+                }
+            } while (shouldContinue);
+        }
+        static double GetNumber()
+        {
+            double a;
+            bool isNumber = true;
+
+            do
+            {
+                string number;
+
+                Console.WriteLine("Введите число:");
+
+                number = Console.ReadLine();
+                isNumber = Double.TryParse(number, out double value);
+
+                if (!isNumber)
+                {
+                    Console.WriteLine("Вы ввели не число, попробуйте снова");
+                    isNumber = true;
+                }
+                else
+                {
+                    Console.WriteLine("Вы успешно ввели число");
+                    isNumber = false;
+                }
+                a = value;
+
+            } while (isNumber);
+
+            return a;
+        }
+    }
+}
