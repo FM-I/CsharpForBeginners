9d5cb3ceb0d1b125ac98aff6e9896a46dd857724
emedvedeva <35913899+emedvedeva@users.noreply.github.com>
2021-01-23 08:21:29 +0300

    Update Program.cs

--- SortBlackJack/Program.cs
+++ SortBlackJack/Program.cs
@@ -1,27 +1,132 @@
 ﻿using System;
+using System.Text;
 
-namespace SortBlackJack
+namespace BlackJack
 {
     class Program
     {
-        private static int[] Shuffle(int[] cards)
+        static void Main(string[] args)
         {
-            int[] result = new int[cards.Length];
+            Console.OutputEncoding = Encoding.UTF8;
 
-            Array.ConstrainedCopy(cards, 0, result, 0, cards.Length);
+            int[] cards = new int[]
+            {
+                2, 3, 4, 5, 6, 7, 8, 9, 10, // - цифры
+                10, 10, 10, // - картинки
+                11, // - туз
 
-            Random random = new Random();
+                2, 3, 4, 5, 6, 7, 8, 9, 10, // - цифры
+                10, 10, 10, // - картинки
+                11, // - туз
+
+                2, 3, 4, 5, 6, 7, 8, 9, 10, // - цифры
+                10, 10, 10, // - картинки
+                11, // - туз
+
+                2, 3, 4, 5, 6, 7, 8, 9, 10, // - цифры
+                10, 10, 10, // - картинки
+                11, // - туз
+            };
+
+            cards = Shuffle(cards);
 
             for (int i = 0; i < cards.Length; i++)
             {
-                
-                int index = random.Next(0, cards.Length-i);
-                int temp = result[index];
-
-                result[index] = result[cards.Length - index]; //переносим выбранный элемент в конец массива
-                result[cards.Length - index] = temp ;
-                result[i] = cards[index];
-                
+                Console.WriteLine(cards[i]);
             }
+
+            Console.WriteLine();
+
+            //cards = Shuffle(cards);
+            //cards = Shuffle(cards);
+
+            //bool isPlayerContinue = true;
+            //bool isComputerContinue = true;
+
+            //Random random = new Random();
+
+            //do
+            //{
+            //    isPlayerContinue = AskPlayer(isPlayerContinue);
+
+            //    isComputerContinue = AskComputer(isComputerContinue, random);
+
+            //} while (isPlayerContinue || isComputerContinue);
         }
+
+        private static int[] Shuffle(int[] cards)
+        {
+            Random random = new Random();
+
+            int l = cards.Length;
+
+            for (int i = 0; i < l; i++)
+            {
+                int index = random.Next(0, l - i);
+                int temp = cards[index];
+
+                cards[index] = cards[l - index - 1]; //переносим выбранный элемент в конец массива
+                cards[l - index - 1] = temp;
+            }
+
+            return cards;
+        }
+
+        private static bool Contains(int[] values, int value)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] == value)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool AskComputer(bool isComputerContinue, Random random)
+        {
+            int decisionNumber = 0;
+
+            if (isComputerContinue)
+            {
+                decisionNumber = random.Next(0, 2);
+            }
+
+            if (decisionNumber == 0)
+            {
+                Console.WriteLine("Компьютер тянет карту");
+            }
+
+            if (decisionNumber == 1)
+            {
+                Console.WriteLine("Компьютер пасует");
+                isComputerContinue = false;
+            }
+
+            return isComputerContinue;
+        }
+
+        private static bool AskPlayer(bool isPlayerContinue)
+        {
+            Console.WriteLine("Будешь тянуть карту? (y/n)");
+            string answer = Console.ReadLine();
+
+            if (answer == "y")
+            {
+                Console.WriteLine("Вы тянете карту");
+            }
+
+            if (answer == "n")
+            {
+                Console.WriteLine("Вы пасуете");
+                isPlayerContinue = false;
+            }
+
+
+
+            return isPlayerContinue;
+        }
+    }
 }
